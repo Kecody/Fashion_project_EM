382530021f9a79e1c4bffcf18a3893d7eca7e977
Kevin.Q <k.queyroi@gmail.com>
2022-03-12 10:56:58 +0100

    method modified

--- appmode/database/seeders/ProductSeeder.php
+++ appmode/database/seeders/ProductSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Database\Factories\ProductFactory;
 
 class ProductSeeder extends Seeder
 {
@@ -14,31 +18,34 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        $genres = Category::all("id");
-    
-        $countCategory = count($categoriesId);
-
-        Product::factory()
-               ->count(10)
-               ->create()
-               ->each (function($product) use ($categoriesId, $countCategory) {
-                    $rand = random_int(2, $countCategory);
-                    // On utilise la collection = méthodes Laravel pour traiter les entités comme des tableaux 
-                    // shuffle mélange le tableau d'entités, slice permet de découper de manière aléatoire ici
-
-                    $categoriesShuffle = $categoriesId->shuffle()->slice($rand);
-
-                    // Pour la méthode attach nous devons passer des ids sous forme d'un tableau
-                    $ids = [];
-                    foreach($categoriesShuffle as $k => $array){
-                        $ids[] = $array["id"];
-                    }
-                    
-                    $product->categories()->attach($ids);
-                    
-                    $product->save();
-                    
-               });
-    
+         // variables du contexte
+         $categoryFemme = Category::where('sex', 'femme')->first();
+         $categoryHomme = Category::where('sex', 'homme')->first();
+       
+         Product::factory()
+         ->count(10)
+         ->create()
+         ->each(function($product) use($categoryFemme, $categoryHomme){
+            //  on va chercher avec random_int l'id pour la category homme et femme
+             $id = random_int(0,1) == 1 ? $categoryFemme->id : $categoryHomme->id;
+            //  on fait l'association de product et category
+             $product->categories()->attach([$id]);
+ 
+             $product->save();
+         });
     }
 }
+
+
+
+// $categoriesId = Category::all('id');
+// $countCategory = count($categoriesId);
+// $rand = random_int(2, 2);
+// // On utilise la collection = méthodes Laravel pour traiter les entités comme des tableaux 
+// // shuffle mélange le tableau d'entités, slice permet de découper de manière aléatoire ici
+// $categoriesShuffle = $categoriesId->shuffle()->slice($rand);
+// // Pour la méthode attach nous devons passer des ids sous forme d'un tableau
+// $ids = [];
+// foreach($categoriesShuffle as $k => $array){
+//     $ids[] = $array["id"];
+// }
